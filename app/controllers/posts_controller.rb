5d85487dc5bc0b2ca827e16170b997c3a6732801
Marc Salo <marcsalo455@gmail.com>
2016-06-16 16:39:12 -0400

    1 col works and saves via AJAX

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
   def index
-    @posts = Post.all
+    @posts = Post.all.order(sort: :asc)
   end
 
   def show
@@ -19,6 +20,24 @@ class PostsController < ApplicationController
 
     @post.save
     redirect_to @post
+  end
+
+  def order
+    puts params[:myOrder]
+
+    ourData = params[:myOrder]
+    puts ourData
+    ourData.each_with_index do |id, i|
+      @post = Post.find(id)
+      @post.sort = (i+1)
+      @post.save
+    end
+
+    render :json => { }, :status => 200
+  end
+
+  def ordertest
+    puts params
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  match '/posts/order' => 'posts#order', :via => :post
   resources :posts
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
